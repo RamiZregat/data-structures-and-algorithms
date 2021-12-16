954b70750c97aa13ee8dc9f8cb40d042e7f13167
RamiZregat <rami.zregat97@gmail.com>
2021-12-16 14:03:58 +0200

    done

--- hashTable/__test__/hashTable.test.js
+++ hashTable/__test__/hashTable.test.js
@@ -1,6 +1,7 @@
 'use strict';
 const Node  = require('../../linked-list/Node');
 const {Hashmap}=require('../hashTable');
+const {BT} =require('../../trees/Binary-tree');
 
 describe('Hash Map', () => {
   test('should create a hash map with pre defined size', () => {
@@ -65,4 +66,46 @@ describe('Hash Map Repeated Words', () => {
     const hashMap = new Hashmap(5000);
     expect(hashMap.repeatedWord(case3)).toBe('summer');
   });
+});
+
+describe('Binary Tree Insertion', () => {
+  const one = new Node(1);
+  const two = new Node(2);
+  const three = new Node(3);
+  const four = new Node(4);
+  const five = new Node(5);
+  const six = new Node(6);
+  const seven = new Node(7);
+  const one2 = new Node(1);
+  const two2 = new Node(2);
+  const three2 = new Node(3);
+  const four2 = new Node(4);
+  const seven2 = new Node(7);
+  const eight = new Node(8);
+  const nine = new Node(9);
+  const tree1 = new BT(one);
+  one.left = two;
+  one.right = three;
+  two.left = four;
+  two.right = five;
+  three.left = six;
+  three.right = seven;
+  const tree2 = new BT(one2);
+  one2.left = two2;
+  one2.right = three2;
+  two2.left = four2;
+  two2.right = eight;
+  three2.left = nine;
+  three2.right = seven2;
+
+
+  test('Find common values in 2 binary trees.', () => {});
+  const expected = [1, 2, 4, 3, 7];
+  const hashMap = new Hashmap(4000);
+  const arr = hashMap.treeIntersection(tree1, tree2);
+  expect(arr[0]).toBe(expected[0]);
+  expect(arr[1]).toBe(expected[1]);
+  expect(arr[2]).toBe(expected[2]);
+  expect(arr[3]).toBe(expected[3]);
+  expect(arr[4]).toBe(expected[4]);
 });
---
+++ hashTable/treeIntersection.md
@@ -0,0 +1,40 @@
+# Hashmap Tree Intersection
+
+## Challenge
+
+- Write a function called tree intersection
+  - It takes two `binary trees`
+  - It returns an array of common values between the two trees
+
+## Whiteboard Process
+
+![challenge-32](tree-intersection.jpg)
+
+## Approach & Efficiency
+
+### Approach
+
+1. I understood the problem first.
+1. I imagined how the results should be.
+1. I made a drawings of how the process of reaching the solution will be.
+1. I wrote the algorithm.
+1. I wrote the code.
+1. I made the tests.
+
+### Efficiency
+
+- Space :
+  - O(n)
+- Time :
+  - O(n)
+
+## API
+
+### How to Use
+
+- To check for intersections between two trees :
+  - `<name-of-hash-map>.treeIntersection(<tree1>, <tree2>)`
+
+### Test
+
+- `npm run test`
